398977d8a115e73a98f3c533b21ed6cf22ac4ece
Alper Tabak <mehmetalpertabak@gmail.com>
2018-04-30 11:49:14 +0300

    fizzbuzz algorithm

---
+++ fizzbuzz/app.js
@@ -0,0 +1,14 @@
+let fizzbuzz = (num) => {
+    for (let i = 1; i <= num; i++){
+        if(i % 15 === 0)
+            console.log("fizzbuzz");
+        else if(i % 3 === 0)
+            console.log("fizz");
+        else if(i % 5 === 0)
+            console.log("buzz");
+        else
+            console.log(i);
+    }
+}
+
+fizzbuzz(30);
